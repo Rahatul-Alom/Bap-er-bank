9236bba564cc9b3d37ba5b01d95cf0b851e54340
Your NameRahatul-Alom <muhammadrahatulalom@gmail.com>
2023-08-18 06:32:55 +0600

    added validation

--- JS/withdraw.js
+++ JS/withdraw.js
@@ -5,26 +5,39 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmmountString = withdrawField.value;
     const newWithdrawAmmount = parseFloat(newWithdrawAmmountString);
 
+    // step-5: clear input field
+        withdrawField.value = '';
+
+    if(isNaN(newWithdrawAmmount)){
+        alert('Please provide a number');
+        return;
+    }    
+
     // step-3: get previous withdraw total
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString)
 
-    // step-4: calculate total withdraw ammount and set total withdraw ammount
-
-    const currntWithdrawTotal = previousWithdrawTotal+ newWithdrawAmmount;
-    withdrawTotalElement.innerText = currntWithdrawTotal;
-
-    // step-5: get the previous balance total
+    // step-4: get the previous balance total
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString)
+
+    // step-5: clear input field
+      withdrawField.value = '';
+
+    if(newWithdrawAmmount > previousBalanceTotal ){
+        alert('Baper bank e eto taka nai');
+        return;
+    }
+    
+     // step-6: calculate total withdraw ammount and set total withdraw ammount
+
+        const currntWithdrawTotal = previousWithdrawTotal+ newWithdrawAmmount;
+        withdrawTotalElement.innerText = currntWithdrawTotal;
 
     // step-6 calculate new balance total and set new balance
     const newbalanceTotal = previousBalanceTotal - newWithdrawAmmount;
     balanceTotalElement.innerText = newbalanceTotal;
 
-
-    // step-7: clear input field
-    withdrawField.value = '';
 })
